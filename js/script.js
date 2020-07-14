21ff74ce334d6dd4a7ddfb4d7049b09162484072
Andrea <miranda.andrea.nd@gmail.com>
2020-07-14 16:50:21 +0200

    inizio jquery

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log("hello");
+
+function addSendListener() {
+  var target = $("#input-chat");
+  target.keyup(sendKeyup);
+}
+
+function sendKeyup(event) {
+var key = event.which;
+
+  if (key==13) {
+    var input = $("#input-chat");
+    var text = input.val();
+    input.val("");
+    console.log(text, "testo");
+  }
+}
+
+function init() {
+
+  addSendListener();
+
+}
+
+$(document).ready(init);
